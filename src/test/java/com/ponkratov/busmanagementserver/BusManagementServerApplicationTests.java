a27c419f6c6cfbf2729c42c1580ca179de1d61c0
Aliaksei Pankratau <super.ponkrat2014@yandex.ru>
2022-03-23 23:40:43 +0300

    Initial commit

---
+++ src/test/java/com/ponkratov/busmanagementserver/BusManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponkratov.busmanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
