e22cd26a6623f844b51cfc7b41cb9750e334c2e2
Aliaksei Pankratau <super.ponkrat2014@yandex.ru>
2022-04-06 00:51:31 +0300

    Created for JWT usage

---
+++ src/main/java/com/ponkratov/busmanagementserver/security/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.ponkratov.busmanagementserver.security;
+
+import com.ponkratov.busmanagementserver.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+}
