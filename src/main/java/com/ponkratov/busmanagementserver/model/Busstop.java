8cb41b3c9299d80ffdda6c6c9d44377872f19e18
Aliaksei Pankratau <super.ponkrat2014@yandex.ru>
2022-04-26 23:20:34 +0300

    Refactored

--- src/main/java/com/ponkratov/busmanagementserver/model/Busstop.java
+++ src/main/java/com/ponkratov/busmanagementserver/model/Busstop.java
@@ -1,12 +1,16 @@
 package com.ponkratov.busmanagementserver.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
 public class Busstop {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -16,9 +20,6 @@ public class Busstop {
     @Column(name = "stopName")
     private String stopName;
     @Basic
-    @Column(name = "cityId")
-    private long cityId;
-    @Basic
     @Column(name = "intermediate")
     private boolean intermediate;
     @Basic
@@ -27,88 +28,21 @@ public class Busstop {
     @Basic
     @Column(name = "longitude")
     private double longitude;
-    @JsonIgnore
+
     @ManyToOne
-    @JoinColumn(name = "cityId", referencedColumnName = "cityId", nullable = false, insertable = false, updatable = false)
+    @JoinColumn(name = "cityId", referencedColumnName = "cityId")
     private City cityByCityId;
-    @JsonIgnore
-    @OneToMany(mappedBy = "busstopByStopId")
-    private Collection<Routebusstop> routebusstopsByStopId;
-
-    public long getStopId() {
-        return stopId;
-    }
-
-    public void setStopId(long stopId) {
-        this.stopId = stopId;
-    }
-
-    public String getStopName() {
-        return stopName;
-    }
-
-    public void setStopName(String stopName) {
-        this.stopName = stopName;
-    }
-
-    public long getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(long cityId) {
-        this.cityId = cityId;
-    }
-
-    public boolean isIntermediate() {
-        return intermediate;
-    }
-
-    public void setIntermediate(boolean intermediate) {
-        this.intermediate = intermediate;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Busstop busstop = (Busstop) o;
-        return stopId == busstop.stopId && cityId == busstop.cityId && intermediate == busstop.intermediate && Double.compare(busstop.latitude, latitude) == 0 && Double.compare(busstop.longitude, longitude) == 0 && Objects.equals(stopName, busstop.stopName);
+        return stopId == busstop.stopId && intermediate == busstop.intermediate && Double.compare(busstop.latitude, latitude) == 0 && Double.compare(busstop.longitude, longitude) == 0 && Objects.equals(stopName, busstop.stopName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(stopId, stopName, cityId, intermediate, latitude, longitude);
-    }
-
-    public City getCityByCityId() {
-        return cityByCityId;
-    }
-
-    public void setCityByCityId(City cityByCityId) {
-        this.cityByCityId = cityByCityId;
-    }
-
-    public Collection<Routebusstop> getRoutebusstopsByStopId() {
-        return routebusstopsByStopId;
-    }
-
-    public void setRoutebusstopsByStopId(Collection<Routebusstop> routebusstopsByStopId) {
-        this.routebusstopsByStopId = routebusstopsByStopId;
+        return Objects.hash(stopId, stopName, intermediate, latitude, longitude);
     }
 }
